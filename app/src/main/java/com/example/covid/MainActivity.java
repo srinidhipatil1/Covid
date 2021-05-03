a06693858ef1a8e9301b6751151fcfff342c57aa
srinidhipatil1 <srinidhi.patil.39@gmail.com>
2021-05-03 11:03:59 +0530

    edittext passed in 2nd activity

--- app/src/main/java/com/example/covid/MainActivity.java
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -4,20 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private CheckBox Fever, Cough, Breathlessness;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button= (Button) findViewById(R.id.button);
-        button.setOnClickListener(v -> openMainActivity2());
-    }
-    public void openMainActivity2(){
-        Intent intent=new Intent(this, MainActivity2.class);
-        startActivity(intent);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edtTextName = (EditText) findViewById(R.id.edtTextName);
+                String s=edtTextName.getText().toString();
+                System.out.println(s+"hellooooo");
+                Intent intent=new Intent(getApplicationContext(), MainActivity2.class);
+                intent.putExtra("stringkey", s);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/covid/MainActivity2.java
+++ app/src/main/java/com/example/covid/MainActivity2.java
@@ -2,13 +2,23 @@ package com.example.covid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity2 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+       TextView tv=(TextView) findViewById(R.id.textView2);
+        Intent intent=getIntent();
+        String str= intent.getStringExtra("stringkey");
+        tv.setText(str);
     }
+
 }
